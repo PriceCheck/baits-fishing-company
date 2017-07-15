cb793cc16f133724ebe9910b25dde60d189e2ece
TuckerBane <Tucker.bane@icloud.com>
2017-07-15 11:48:27 -0700

    mouse mouse mouse

---
+++ BaitsFishingCompany/Assets/Scripts/BeTheMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeTheMouse : MonoBehaviour {
+    public GameObject globals;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = globals.GetComponent<Raycaster>().mousePosition;
+        transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+	}
+}
---
+++ BaitsFishingCompany/Assets/Scripts/Raycaster.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycaster : MonoBehaviour {
+    Ray ray;
+    RaycastHit hit;
+    GameObject LastObjectHit;
+    public bool objectHovering = false;
+    public Vector3 mousePosition;
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (LastObjectHit != hit.collider.gameObject)
+            {
+                objectHovering = false;
+                //if (LastObjectHit != null)
+                //{
+                //    //Unhighlight last target
+                //    if (LastObjectHit.GetComponent<Target>())
+                //    { LastObjectHit.GetComponent<Target>().OnLowlit();
+                //    }
+                //}
+                //Highlight new target
+                LastObjectHit = hit.collider.gameObject;
+                //if (LastObjectHit.GetComponent<Target>())
+                //{ LastObjectHit.GetComponent<Target>().OnHighlit();
+                //    objectHovering = true;
+                //}
+            }
+            LastObjectHit = hit.collider.gameObject;
+            mousePosition = hit.point;
+        }
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    if (LastObjectHit != null)
+        //    {
+        //        //Fire at target
+        //        if (LastObjectHit.GetComponent<Target>())
+        //        { LastObjectHit.GetComponent<Target>().OnShot(); }
+        //    }
+
+        //}
+    }
+}
