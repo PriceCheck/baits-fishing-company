06448d50ad01e3f8fa147521b423573a6c11e843
PriceCheck <taylor.riviera@digipen.edu>
2017-07-15 21:12:17 -0700

    Added acceleration

--- BaitsFishingCompany/Assets/Scripts/BoatController.cs
+++ BaitsFishingCompany/Assets/Scripts/BoatController.cs
@@ -5,14 +5,17 @@ using com.ootii.Messages;
 
 public class BoatController : MonoBehaviour {
     public GameObject Monster;
-    public float moveSpeed = 3;
-    public float maxSpeed = 5;
+    float maxSpeed = 10;
     public float angularMoveSpeed = 180;
     public float drag = 0.9f;
 
     public float tightDistance = 4;
     public float maxDistance = 4.1f;
 
+    public AnimationCurve AccelerationRate = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    float currentSpeed = 0;
+    float timeTilMaxSpeed = 0.8f;
+    
 
     Rigidbody myRigidbody;
 	// Use this for initialization
@@ -31,6 +34,11 @@ public class BoatController : MonoBehaviour {
     }
     // Update is called once per frame
     void Update () {
+        float leftEngine = Input.GetKey(KeyCode.A) ? 1 : 0;
+        float rightEngine = Input.GetKey(KeyCode.D) ? 1 : 0;
+        currentSpeed += (leftEngine * Time.deltaTime) - (rightEngine * Time.deltaTime);
+        currentSpeed *= drag;
+        currentSpeed = Mathf.Clamp(currentSpeed, -timeTilMaxSpeed, timeTilMaxSpeed);
 
         Vector3 pos = transform.position;
         pos.z = 1;
@@ -58,17 +66,8 @@ public class BoatController : MonoBehaviour {
 
             //Vector3 toLookAt = Monster.transform.position;
             //toLookAt.z = transform.position.z;
-            //transform.LookAt(toLookAt);
-
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                LineTight(true);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                LineTight(false);
-            }
+            //transform.LookAt(toLookAt
+            LineTight();
         }
         else
         {
@@ -79,30 +78,24 @@ public class BoatController : MonoBehaviour {
     void LineLoose()
     {
         Vector3 moveDirection = Vector3.zero;
-        float leftEngine = Input.GetKey(KeyCode.A) ? 1 : 0;
-        float rightEngine = Input.GetKey(KeyCode.D) ? 1 : 0;
-        moveDirection.y += leftEngine * moveSpeed;
-        moveDirection.y -= rightEngine * moveSpeed;
+
+        float direction = currentSpeed < 0 ? -1 : 1;
+        moveDirection.y = AccelerationRate.Evaluate(Mathf.Abs(currentSpeed) / timeTilMaxSpeed) * direction;
 
         moveDirection = transform.TransformDirection(moveDirection);
 
-        myRigidbody.velocity *= drag;
-        myRigidbody.velocity += moveDirection;
-        if (myRigidbody.velocity.magnitude > maxSpeed)
-        {
-            myRigidbody.velocity = myRigidbody.velocity.normalized * maxSpeed;
-        }
+        transform.position += moveDirection * maxSpeed * Time.deltaTime;
+   
     }
-    void LineTight(bool goClockwise)
+    void LineTight()
     {
-
         myRigidbody.velocity = Vector3.zero;
 
         Vector3 monsterLoc = Monster.transform.position;
         Vector3 playerLoc = transform.position;
         Vector3 connectingVector = playerLoc - monsterLoc;
-        float direction = goClockwise ? 1.0f : -1.0f;
+        float direction = currentSpeed < 0 ? -1 : 1;
 
-        transform.position = (Quaternion.AngleAxis(angularMoveSpeed * Time.deltaTime * direction, Vector3.back) * connectingVector) + monsterLoc;
+        transform.position = (Quaternion.AngleAxis(angularMoveSpeed * AccelerationRate.Evaluate(Mathf.Abs(currentSpeed) / timeTilMaxSpeed) * Time.deltaTime * direction, Vector3.back) * connectingVector) + monsterLoc;
     }
 }
