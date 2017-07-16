1bd59fc7b2d5b2b14aa1103bbb227bdf2b336591
tucker.bane@icloud.com <Tucker.bane@icloud.com>
2017-07-15 19:40:28 -0700

    Boat movement is a go go

--- BaitsFishingCompany/Assets/Scripts/BoatController.cs
+++ BaitsFishingCompany/Assets/Scripts/BoatController.cs
@@ -9,31 +9,83 @@ public class BoatController : MonoBehaviour {
     public float maxSpeed = 5;
     public float angularMoveSpeed = 180;
     public float drag = 0.9f;
+
+    public float tightDistance = 4;
+    public float maxDistance = 4.1f;
+
+
     Rigidbody myRigidbody;
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.A))
+
+    float DistanceTo(GameObject obj)
+    {
+        return (transform.position - obj.transform.position).magnitude;
+    }
+
+    Vector3 VectorTo(Vector3 vec)
+    {
+        return (vec - transform.position).normalized;
+    }
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 pos = transform.position;
+        pos.z = 1;
+        transform.position = pos;
+
+        if( DistanceTo(Monster) > maxDistance )
         {
-            LineTight(true);
+            transform.position = Monster.transform.position - VectorTo(Monster.transform.position) * maxDistance;
         }
-        else if(Input.GetKey(KeyCode.D))
+
+        if (DistanceTo(Monster) >= tightDistance)
         {
-            LineTight(false);
+
+            var dir = Monster.transform.position - transform.position;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            //Quaternion Rot = Quaternion.LookRotation(Monster.transform.position - transform.position);
+            //print(Rot);
+            //Rot = Quaternion.Euler(new Vector3(0f, 1f, 0f));
+            ////print(Rot);
+            //print(Rot.eulerAngles);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Rot, 1);
+            //print(transform.rotation);
+
+            //Vector3 toLookAt = Monster.transform.position;
+            //toLookAt.z = transform.position.z;
+            //transform.LookAt(toLookAt);
+
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                LineTight(true);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                LineTight(false);
+            }
+        }
+        else
+        {
+            LineLoose();
         }
     }
 
     void LineLoose()
     {
         Vector3 moveDirection = Vector3.zero;
-        float leftEngine = Input.GetKey(KeyCode.A) ? 1 : -1;
-        float rightEngine = Input.GetKey(KeyCode.D) ? 1 : -1;
-        moveDirection.x -= leftEngine * moveSpeed;
-        moveDirection.x += rightEngine * moveSpeed;
+        float leftEngine = Input.GetKey(KeyCode.A) ? 1 : 0;
+        float rightEngine = Input.GetKey(KeyCode.D) ? 1 : 0;
+        moveDirection.y += leftEngine * moveSpeed;
+        moveDirection.y -= rightEngine * moveSpeed;
+
+        moveDirection = transform.TransformDirection(moveDirection);
+
         myRigidbody.velocity *= drag;
         myRigidbody.velocity += moveDirection;
         if (myRigidbody.velocity.magnitude > maxSpeed)
@@ -43,12 +95,14 @@ public class BoatController : MonoBehaviour {
     }
     void LineTight(bool goClockwise)
     {
+
+        myRigidbody.velocity = Vector3.zero;
+
         Vector3 monsterLoc = Monster.transform.position;
         Vector3 playerLoc = transform.position;
         Vector3 connectingVector = playerLoc - monsterLoc;
         float direction = goClockwise ? 1.0f : -1.0f;
 
         transform.position = (Quaternion.AngleAxis(angularMoveSpeed * Time.deltaTime * direction, Vector3.back) * connectingVector) + monsterLoc;
-        print(transform.position);
     }
 }
